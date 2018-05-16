06bee4473a25e2724ebe5ac4deb76c26c3662665
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-05-15 17:22:51 -0700

    Display account balance in CLI
    `chainlink account`
    
    [Finishes #155879137]

--- store/presenters/presenters.go
+++ store/presenters/presenters.go
@@ -56,14 +56,14 @@ func ShowLinkBalance(store *store.Store) (string, error) {
 	if err != nil {
 		return "", err
 	}
+
 	address := account.Address
 	linkContractAddress := common.HexToAddress(store.Config.LinkContractAddress)
-	balance, err := store.TxManager.GetERC20Balance(address, linkContractAddress)
+	linkBalance, err := store.TxManager.GetLinkBalance(address, linkContractAddress)
 	if err != nil {
 		return "", err
 	}
-	// Because Eth and Link both use 1e18 precision, we can correct using the same facility
-	linkBalance := utils.WeiToEth(balance)
+
 	result := fmt.Sprintf("Link Balance for %v: %v", address.Hex(), linkBalance.FloatString(18))
 	return result, nil
 }
@@ -80,6 +80,32 @@ func (bt BridgeType) MarshalJSON() ([]byte, error) {
 		Alias
 	}{
 		Alias(bt),
+	})
+}
+
+// AccountBalance holds the hex representation of the address plus it's ETH & LINK balances
+type AccountBalance struct {
+	Address     string   `json:"address"`
+	EthBalance  *big.Rat `json:"eth_balance"`
+	LinkBalance *big.Rat `json:"link_balance"`
+}
+
+func (a AccountBalance) GetID() string {
+	return a.Address
+}
+
+func (a *AccountBalance) SetID(value string) error {
+	a.Address = value
+	return nil
+}
+
+// MarshalJSON returns the JSON byte encoding of the AccountBalance
+func (a AccountBalance) MarshalJSON() ([]byte, error) {
+	type Alias AccountBalance
+	return json.Marshal(&struct {
+		Alias
+	}{
+		Alias(a),
 	})
 }
 
